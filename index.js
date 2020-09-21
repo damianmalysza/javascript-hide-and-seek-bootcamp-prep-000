a71edd35ec9207a04010b2824b7d24b168435979
Damian Malysza <damian.malysza@gmail.com>
2020-09-21 18:47:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function getFirstSelector(selector){
+  return document.querySelector(selector)
+}
 
+function nestedTarget(){
+  return document.getElementById("nested").querySelector(".target")
+}
+
+function increaseRankBy(n){
+  var rankList=document.querySelectorAll(".ranked-list li")
+  for (let i=0;i<rankList.length;i++){
+    rankList[i].innerHTML=(parseInt(rankList[i].innerHTML)+n)
+  }
+}
+
+function deepestChild(){
+  var current=document.querySelectorAll("#grand-node div")
+  var lastValue=current[current.length-1]
+  return lastValue
+}
