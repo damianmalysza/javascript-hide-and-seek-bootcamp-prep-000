b468b42d3a7417fa59b6d89cc89e54570a80f186
Damian Malysza <damian.malysza@gmail.com>
2020-09-21 18:18:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ function nestedTarget(){
 function increaseRankBy(n){
   var rankList=document.querySelectorAll(".ranked-list li")
   for (let i=0;i<rankList.length;i++){
-    rankList[i]=(parseInt(rankList[i])+n)
+    rankList[i]=(parseInt(rankList[i].innerHTML)+n)
   }
 }
